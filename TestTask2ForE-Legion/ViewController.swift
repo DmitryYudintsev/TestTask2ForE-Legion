56d072c496f7561c8c19f453863de03293b01662
yudintsev.dv <dyudintsev@gmail.com>
2020-10-18 00:59:52 +0200

    Initial Commit

---
+++ TestTask2ForE-Legion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask2ForE-Legion
+//
+//  Created by Дмитрий Юдинцев on 18.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
